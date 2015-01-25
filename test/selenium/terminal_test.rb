b94bd6843783f3a166e5634593137f3a2d5592a1
Pete Kinnecom <pete.kinnecom@appfolio.com>
2015-01-25 15:58:36 -0800

    Rough draft of homebuilt textfield

--- test/selenium/terminal_test.rb
+++ test/selenium/terminal_test.rb
@@ -5,18 +5,15 @@ class TerminalTest < PortfolioSeleniumTest
   attr_accessor :page; private :page
 
   def test_loads_page
-    assert_equal "Article A", page.title.text
+    page.terminal.input.run("article koutouki")
 
-    page.terminal.input.run("article b")
-    assert_equal "Article B", page.title.text
-
-    assert_equal "Routing to article b", page.terminal.outputs.last.text
+    assert_equal "Routing to article koutouki", page.terminal.outputs.last.text
+    assert_equal "Koutouki.org", page.title.text
   end
 
   def test_moves_caret
     input = page.terminal.input
 
-    #TODO: show caret on page load
     #assert_equal '', input.caret_char
     input.type('abc')
     assert_equal '', input.caret_char
@@ -24,20 +21,21 @@ class TerminalTest < PortfolioSeleniumTest
     input.type(:left)
     assert_equal 'c', input.caret_char
 
-    #input.arrow(:left)
-    #assert_equal 'b', input.caret_char
+    input.type(:left)
+    assert_equal 'b', input.caret_char
 
-    #input.type('b')
+    input.type('d')
 
     # Should be this, but I think we get
     # a refocus event, so we're out of luck
     # The caret is moved to the end on refocus
     # and it doesn't work :(
-    #assert_equal 'a', input.caret_char
-    #assert_equal 'ba', input.text
+    assert_equal 'b', input.caret_char
+    assert_equal 'adbc', input.text
 
-    input.type(:up)
-    assert_equal 'a', input.caret_char
+    # TODO: implement up
+    #input.type(:up)
+    #assert_equal 'a', input.caret_char
 
     input.type(:enter)
     assert_equal '', input.text
