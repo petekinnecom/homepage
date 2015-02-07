9c9ebfa3e4d67a08d38b15daffa4e0e4de5a1222
Pete Kinnecom <pete.kinnecom@appfolio.com>
2015-02-07 15:20:36 -0800

    Implement up, down arrows

--- test/selenium/prompt_test.rb
+++ test/selenium/prompt_test.rb
@@ -1,11 +1,10 @@
 require 'selenium_helper'
 
-class TerminalTest < PortfolioSeleniumTest
+class PromptTest < PortfolioSeleniumTest
 
   attr_accessor :page; private :page
 
   def test_loads_page
-    page.terminal.input.node.click
     page.terminal.input.run("article koutouki")
 
     assert_eventually_equal "Loaded article koutouki", proc { page.terminal.outputs.last.text }
@@ -15,9 +14,8 @@ class TerminalTest < PortfolioSeleniumTest
   def test_moves_caret
     input = page.terminal.input
     input.node.click
-    sleep 1
 
-    #assert_equal '', input.caret_char
+    assert_eventually_equal '', proc { input.caret_char }
     input.type('abc')
     assert_equal '', input.caret_char
 
@@ -29,10 +27,6 @@ class TerminalTest < PortfolioSeleniumTest
 
     input.type('d')
 
-    # Should be this, but I think we get
-    # a refocus event, so we're out of luck
-    # The caret is moved to the end on refocus
-    # and it doesn't work :(
     assert_equal 'b', input.caret_char
     assert_equal 'adbc', input.text
 
@@ -41,10 +35,6 @@ class TerminalTest < PortfolioSeleniumTest
 
     input.type(:right)
     assert_equal '', input.caret_char
-
-    # TODO: implement up
-    #input.type(:up)
-    #assert_equal 'a', input.caret_char
 
     input.type(:enter)
     assert_equal '', input.text
@@ -83,10 +73,50 @@ class TerminalTest < PortfolioSeleniumTest
     assert_equal '', input.caret_char
   end
 
+  def test_history_of_input
+    input = page.terminal.input
+
+    input.type('first')
+    input.type(:enter)
+    assert_eventually_equal '', proc { input.text }
+    assert input.visible?, "Wait until it reappears"
+
+    input.type('second')
+    input.type(:enter)
+    assert_eventually_equal '', proc { input.text }
+    assert input.visible?, "Wait until it reappears"
+
+
+    input.type('third')
+    input.type(:enter)
+    assert_eventually_equal '', proc { input.text }
+    assert input.visible?, "Wait until it reappears"
+
+    input.type(:up)
+    assert_equal 'third', input.text
+    input.type(:up)
+    assert_equal 'second', input.text
+    input.type(:up)
+    assert_equal 'first', input.text
+
+    input.type(:down)
+    assert_equal 'second', input.text
+    input.type(:down)
+    assert_equal 'third', input.text
+    input.type(:down)
+    assert_equal '', input.text
+  end
+
   private
 
   def setup
     @page = visit('/')
+    @page.terminal.input.node.click
+
+    #ensure all the animations have finished
+    assert_eventually_equal true, proc {@page.terminal.outputs.visible?}
+    assert_eventually_equal 'Routing to default article', proc { @page.terminal.outputs.last.text }
+    assert_eventually_equal '', proc { @page.terminal.input.text }
   end
 
 end
